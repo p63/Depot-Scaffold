245fa48010ad6b7719d203242e5e769e2fd81d79
p63 <contact@christopheraedwards.com>
2014-02-17 17:31:23 -0800

    Task c

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -31,12 +31,13 @@ end
 test "image_url" do
    ok=%w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
    bad=%w{ fred.doc fred.gif/more fred.gif.more}
+end
    ok.each do |name|
    assert new_product(name).valid?, "#{name} should be valid"
 end
 bad.each do |name|
   assert new_product(name).invalid? "#{name} shouldn't be valid"
-  end
+end
 
 test "product is not valid without a unique title -i18n" do
   product = Product.new(title: products(:ruby).title,
